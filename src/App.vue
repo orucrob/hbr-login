d25d017c2c137139400cddfee7df30da8fa1d817
Rob <orucrob@gmail.com>
2019-09-03 21:14:18 +0200

    test for api bikes

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,39 @@
             <router-link to="/signout" v-if="user">Signout</router-link>
             <router-link to="/login" v-else>Login</router-link>
         </div>
-        <router-view/>
-        <vue-snotify/>
-        <hr>
+        <router-view />
+        <vue-snotify />
+        <hr />
         <div v-if="user" class="dev-info">
             <div>LOGGED IN</div>
             <div>idToken: {{idToken}}</div>
             <div>accessToken: {{accessToken}}</div>
         </div>
+        <button type="button" @click="onAdminSync">Admin Sync</button>
+        <button type="button" @click="onMyStatus">My Status</button>
+        <button type="button" @click="onAllUsers">All Users</button>for lobby:
+        <input type="text" v-model="allUsersLobby" />
+        <pre>Sync: 
+            {{sync}}</pre>
+        <pre>MyStatus: 
+            {{myStatus}}</pre>
+        <pre>All Users: 
+            {{allUsers}}</pre>
+        <hr />Locks:
+        <button type="button" @click="onAllLocks">Get</button>for lobby:
+        <input type="text" v-model="allLocksLobby" />
+        <pre>{{locksResponse}}</pre>
+        <hr />Lock:
+        <input type="text" v-model="lock" />
+        <button type="button" @click="onGetLock">Get</button>
+        <button type="button" @click="onUnlockTake">Unlock to Take</button>
+        <button type="button" @click="onUnlockReturn">Unlock to Return</button>
+        <pre>Lock Response: {{lockResponse}}</pre>
     </div>
 </template>
 
 <script>
+const APINAME = "BikeAPI";
 
 export default {
     name: "app",
@@ -25,24 +46,136 @@ export default {
         return {
             user: undefined,
             idToken: undefined,
-            accessToken: undefined
+            accessToken: undefined,
+            myStatus: undefined,
+            allUsers: undefined,
+            sync: undefined,
+            allUsersLobby: undefined,
+            lock: undefined,
+            allLocksLobby: undefined,
+            locksResponse: undefined,
+            lockResponse: undefined
         };
     },
     beforeMount: async function() {
         try {
-            this.user = await this.$Amplify.Auth.currentAuthenticatedUser()
-            this.session = await this.$Amplify.Auth.currentSession()
-            if(this.session){
-                this.idToken = this.session.idToken.jwtToken
-                this.accessToken = this.session.accessToken.jwtToken
-            }else{
-                this.idToken = undefined
-                this.accessToken = undefined
+            this.user = await this.$Amplify.Auth.currentAuthenticatedUser();
+            this.session = await this.$Amplify.Auth.currentSession();
+            if (this.session) {
+                this.idToken = this.session.idToken.jwtToken;
+                this.accessToken = this.session.accessToken.jwtToken;
+            } else {
+                this.idToken = undefined;
+                this.accessToken = undefined;
             }
             console.log("app:gotuser", this.session);
         } catch (e) {
             console.log("app:gotuser/session err", e);
             this.user = undefined;
+        }
+    },
+    methods: {
+        async onMyStatus() {
+            try {
+                this.myStatus = await this.$Amplify.API.get(
+                    APINAME,
+                    "/mystatus"
+                );
+            } catch (err) {
+                this.myStatus = JSON.stringify(
+                    (err && err.response && err.response.data) || err,
+                    null,
+                    2
+                );
+                console.log("mystatus error:", this.myStatus);
+            }
+        },
+        async onAdminSync() {
+            try {
+                this.sync = await this.$Amplify.API.get(APINAME, "/sync");
+            } catch (err) {
+                this.sync = JSON.stringify(
+                    (err && err.response && err.response.data) || err,
+                    null,
+                    2
+                );
+                console.log("sync error:", this.sync);
+            }
+        },
+        async onAllUsers() {
+            try {
+                this.allUsers = await this.$Amplify.API.get(
+                    APINAME,
+                    `/users?lobby=${this.allUsersLobby}`
+                );
+            } catch (err) {
+                this.allUsers = JSON.stringify(
+                    (err && err.response && err.response.data) || err,
+                    null,
+                    2
+                );
+                console.log("all users error:", this.allUsers);
+            }
+        },
+        async onAllLocks() {
+            try {
+                this.locksResponse = await this.$Amplify.API.get(
+                    APINAME,
+                    `/locks?lobby=${this.allLocksLobby}`
+                );
+            } catch (err) {
+                this.locksResponse = JSON.stringify(
+                    (err && err.response && err.response.data) || err,
+                    null,
+                    2
+                );
+                console.log("all locks error:", this.locksResponse);
+            }
+        },
+        async onGetLock() {
+            try {
+                this.lockResponse = await this.$Amplify.API.get(
+                    APINAME,
+                    `/locks/${this.lock}`
+                );
+            } catch (err) {
+                this.lockResponse = JSON.stringify(
+                    (err && err.response && err.response.data) || err,
+                    null,
+                    2
+                );
+                console.log("Get Lock error:", this.lockResponse);
+            }
+        },
+        async onUnlockTake() {
+            try {
+                this.lockResponse = await this.$Amplify.API.get(
+                    APINAME,
+                    `/locks/${this.lock}/unlocktotake`
+                );
+            } catch (err) {
+                this.lockResponse = JSON.stringify(
+                    (err && err.response && err.response.data) || err,
+                    null,
+                    2
+                );
+                console.log("unlock to take error:", this.lockResponse);
+            }
+        },
+        async onUnlockReturn() {
+            try {
+                this.lockResponse = await this.$Amplify.API.get(
+                    APINAME,
+                    `/locks/${this.lock}/unlocktoreturn`
+                );
+            } catch (err) {
+                this.lockResponse = JSON.stringify(
+                    (err && err.response && err.response.data) || err,
+                    null,
+                    2
+                );
+                console.log("unlock to return error:", err.response.data);
+            }
         }
     }
 };
--- src/plugins/amplify.js
+++ src/plugins/amplify.js
@@ -1,20 +1,37 @@
-import Vue from 'vue'
-import Amplify, * as AmplifyModules from 'aws-amplify'
-import { AmplifyPlugin } from 'aws-amplify-vue'
+import Vue from 'vue';
+import Amplify, * as AmplifyModules from 'aws-amplify';
+import { AmplifyPlugin } from 'aws-amplify-vue';
 
 Amplify.configure({
-    Auth: {
-        // REQUIRED - Amazon Cognito Region
-        region: 'eu-west-1',
-        // OPTIONAL - Amazon Cognito User Pool ID
-        userPoolId: 'eu-west-1_xy',
-        // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
-        userPoolWebClientId: 'xyz'
-        // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
-        //mandatorySignIn: false,
-        // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
-        //authenticationFlowType: 'USER_PASSWORD_AUTH'
-    }
-})
+  Auth: {
+    // REQUIRED - Amazon Cognito Region
+    region: 'eu-west-1',
+    // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolId: 'eu-west-1_hwx4ceEkM',
+    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    userPoolWebClientId: '1t1a1sqj2u33itbqup8tfptron'
+    // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+    //mandatorySignIn: false,
+    // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
+    //authenticationFlowType: 'USER_PASSWORD_AUTH'
+  },
+  API: {
+    endpoints: [
+      {
+        name: 'BikeAPI',
+        endpoint: 'https://8rbjufwlj3.execute-api.eu-west-1.amazonaws.com/dev',
+        custom_header: async () => {
+          // return { Authorization : 'token' }
+          // Alternatively, with Cognito User Pools use this:
+          return {
+            Authorization: `Bearer ${(await Amplify.Auth.currentSession())
+              .getIdToken()
+              .getJwtToken()}`
+          };
+        }
+      }
+    ]
+  }
+});
 
-Vue.use(AmplifyPlugin, AmplifyModules)
+Vue.use(AmplifyPlugin, AmplifyModules);
